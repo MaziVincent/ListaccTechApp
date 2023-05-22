08695d9d3ae3d04c1903388f6aba9fa541c624c3
Vincent <georgevincent35@gmail.com>
2023-05-22 16:56:12 +0100

    landing page commit

--- ListaccTechAppSPA/src/api/fetchData.ts
+++ ListaccTechAppSPA/src/api/fetchData.ts
@@ -1,55 +1,44 @@
 import axios from "./axios";
 
-const fetchData = async (url:string) => {
 
-    type dataState ={
-        dataList:{
-         currentPage:number,
-         hasNext:boolean,
-         hasPrevious:boolean,
-         pageCount:number,
-         totalCount:number,
-         totalPages:number
-        }
-        result:[]
-     }
+const fetchData = async (url:string, token:string) => {
 
+    
+    const baseUrl = 'https://localhost:7188/api/'
      const controller = new AbortController();
      let errors:any; 
-     let data:dataState = {
-       dataList:{
- 
-     currentPage:0,
-     hasNext:false,
-     hasPrevious:false,
-     pageCount:0,
-     totalCount:0,
-     totalPages:0
- 
-     }, result:[]};
- 
+     let data:any
+
+
      try{
-         const response = await axios.get(url,{
-             signal:controller.signal
+         const response = await axios.get(baseUrl + url,{
+             signal:controller.signal,
+             headers:{
+              Authorization: `Bearer ${token}`
+             }
          });
          
-        //console.log(response.data);
          data = response.data;
-         
- 
+         console.log(data)
+
+      
+
      }catch(error:any){
        errors = error;
  
          console.log(error)
+
+
         
      }
  
      controller.abort();
+
+  
+
+   
      
-     
- 
-       return data;
-     
+  return {data,errors}
  
      
    }
